ec28fa988300c49a8b15c8ccc39a10eb0b93ac05
Soodeh84 <soodeh.mousavi.84@gmail.com>
2023-12-03 12:32:13 +0100

    add clock main body and the cylindrical outer ring

--- Assignment2/Clock3D.js
+++ Assignment2/Clock3D.js
@@ -17,11 +17,80 @@ scene.add( axesHelper );
 
 // Add a camera
 const camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 500 );
-camera.position.set(1,2,5);
+camera.position.set(0,0,7);
 
-// Add the clock here:
+// Add light sources
+scene.add(new THREE.AmbientLight('#909090'));
+const light = new THREE.PointLight();
+light.position.set(10,10,10);
+light.intensity = 200;
+scene.add(light);
 
+/*******************************cylindrical outer ring from ch5/ex2****************************************************/
+// Materials
+const mat = new THREE.MeshStandardMaterial({color:'#afeeee',
+                                            metalness:0.1,
+                                            roughness:0.1,
+                                            flatShading:true,   
+                                            side: THREE.DoubleSide});
+const bodyRadius = 3; //clock body radius
+const outerRadius = 3.5;  // inner radius of the ring
+const height = 0.5;
+const innerRadius = 3;
 
+// a ring with ExtrudeGeometry
+const outerCircle = new THREE.Shape();
+outerCircle.moveTo(outerRadius, 0);
+const innerCircle = new THREE.Shape();   // serves as hole in outerCircle
+innerCircle.moveTo(innerRadius, 0);
+const N =100;
+const deltaPhi = 2*Math.PI / N;
+for(let k=1; k<=N; ++k) {
+  outerCircle.lineTo(outerRadius*Math.cos(k*deltaPhi),
+                     outerRadius*Math.sin(k*deltaPhi));
+  innerCircle.lineTo(innerRadius*Math.cos(k*deltaPhi),
+                     innerRadius*Math.sin(k*deltaPhi));
+}
+outerCircle.holes.push(innerCircle);
+
+const extrudeSettings = {
+  bevelEnabled: false,
+  depth: height,
+};
+const extrudeGeo = new THREE.ExtrudeGeometry(outerCircle, extrudeSettings);
+const extrudeRing = new THREE.Mesh(extrudeGeo, mat);
+scene.add(extrudeRing);
+
+/*******************************************Add the clock here:****************************************************** */
+//clock main body
+const mat1 = new THREE.MeshStandardMaterial({color:'white',
+                                            metalness:0,
+                                            roughness:1,
+                                            flatShading:true,   
+                                            side: THREE.DoubleSide});
+
+// clock body with ExtrudeGeometry
+const clockBody = new THREE.Shape();
+clockBody.moveTo(bodyRadius, 0); 
+
+for(let k=1; k<=N; ++k) {
+  clockBody.lineTo(bodyRadius*Math.cos(k*deltaPhi),
+                   bodyRadius*Math.sin(k*deltaPhi));
+}
+
+const extrudeSettings1 = {
+  bevelEnabled: false,
+  depth: height,
+};
+const extrudeGeo1 = new THREE.ExtrudeGeometry(clockBody, extrudeSettings1);
+const extrudeRing1 = new THREE.Mesh(extrudeGeo1, mat1);
+scene.add(extrudeRing1);
+
+//clock ticks
+//clock hands
+//clock blob
+
+/************************************************************************************************* */
 const controls = new TrackballControls(camera, renderer.domElement);
 // Render the scene
 function render() {
